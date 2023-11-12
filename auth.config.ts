63364e4e7462e14a73b651b35caf5e51e15dbff5
Mateusz Bielak <mt.bielak@gmail.com>
2023-11-12 16:43:57 +0100

    Handle auth

---
+++ auth.config.ts
@@ -0,0 +1,18 @@
+import type { NextAuthConfig } from 'next-auth';
+ 
+export const authConfig = {
+  providers: [],
+  pages: {
+    signIn: '/login',
+  },
+  callbacks: {
+    authorized({ auth, request: { nextUrl } }) {
+      console.log({ pathname: nextUrl.pathname });
+      const isLoggedIn = !!auth?.user;
+      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      if (isOnDashboard) return isLoggedIn;
+      if (isLoggedIn) return Response.redirect(new URL('/dashboard', nextUrl));
+      return true;
+    },
+  },
+} satisfies NextAuthConfig;
